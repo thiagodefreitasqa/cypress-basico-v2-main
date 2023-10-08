d13476d12c44718bd8736c1f824a1291a3800ddb
th.freitas <91982342+thiagodefreitasqa@users.noreply.github.com>
2023-10-08 17:06:30 -0300

    exercicio 1 beforeEach

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -5,4 +5,16 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.visit('./src/index.html')
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
+describe('Nome campo obrigatório',function(){
+    this.beforeEach
+        it('informando o nome do usuario',function(){
+            cy.get('#firstName').should('be.visible').type('Thiago').should('not.have.value','QA')
+            cy.get('#lastName').should('be.visible').type('QA').should('have.value','QA')
+            cy.get('#email').should('be.visible').type('thiagofreitasqatest@gmail.com')
+            cy.get('#open-text-area').should('be.visible').
+            type('O curso é  muito bom do TAT!! :)')
+            cy.get('.button').click()
+        })
+    
   })
+})
