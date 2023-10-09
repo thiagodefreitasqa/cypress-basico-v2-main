734c6cc103e8a2952eacaabca5d886409bbb3fab
th.freitas <91982342+thiagodefreitasqa@users.noreply.github.com>
2023-10-08 21:58:35 -0300

    exercicio_extra5

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,20 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function(){
         cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
-describe('Nome campo obrigatório',function(){
-    this.beforeEach
-        it('informando o nome do usuario',function(){
+    it.only('preenche campos obrigatorios e envia formulario',function(){
             cy.get('#firstName').should('be.visible').type('Thiago').should('not.have.value','QA')
-            cy.get('#lastName').should('be.visible').type('QA').should('have.value','QA')
+            cy.get('#lastName').should('be.visible').type('Freitas')
             cy.get('#email').should('be.visible').type('thiagofreitasqatest@gmail.com')
             cy.get('#open-text-area').should('be.visible').
-            type('O curso é  muito bom do TAT!! :)')
-            cy.get('.button').click()
+            type('{de},O curso é  muito bom do TAT!! :)')
+            cy.get('button[type="submit"]').click()
+
+            cy.get('.success').should('be.visible')
         })
     
   })
-})
