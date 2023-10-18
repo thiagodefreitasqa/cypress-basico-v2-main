dd1a86fd42e340fcce26206c8bee3f4180db78e9
Thiago de Freitas <91982342+thiagodefreitasqa@users.noreply.github.com>
2023-10-18 14:17:57 -0300

    atualizado itens aula 10

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  viewportHeight: 880,
-  viewportWidth: 1280,
+  "pluginFile":false,
+  "viewportHeight": 880,
+  "viewportWidth": 1280,
   e2e: {
     setupNodeEvents(on, config) {},
   },
